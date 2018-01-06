5d0f88f7c99b001234e995b2d366d45e56b1816d
AQ <aq@okaq.com>
2018-01-06 10:51:39 -0500

    Web server for swan

--- equus/awai.go
+++ equus/awai.go
@@ -6,12 +6,23 @@ package main
 
 import (
     "fmt"
+    "net/http"
+    "time"
 )
 
 const (
     INDEX = "zwai.html"
 )
 
-func main() {
-    
+func AwaiHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
+    http.ServeFile(w,r,INDEX)
 }
+
+func main() {
+    fmt.Printf("okaq equus started at\n%s\n", time.Now().String())
+    http.HandleFunc("/", AwaiHandler)
+    http.ListenAndServe(":8080", nil)
+}
+
+
