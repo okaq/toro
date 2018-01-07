3057f59b3553d377a72809c5f5206454cbee5319
AQ <aq@okaq.com>
2018-01-06 20:12:11 -0500

    Eiga image file list endpoint

--- equus/dogu/eiga.go
+++ equus/dogu/eiga.go
@@ -21,9 +21,17 @@ func EigaHandler(w http.ResponseWriter, r *http.Request) {
     http.ServeFIle(w,r,EIGA)
 }
 
+func ListHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
+    // api endpoint design
+    // all app data can be served as json
+    // list png image names in img dir
+}
+
 func main() {
     fmt.Printf("okaq equus eiga start\n%s\n", time.Now().String())
     http.HandleFunc("/", EigaHandler)
+    http.HandleFunc("/list", ListHandler)
     http.ListenAndServe(":8080", nil)
 }
 
