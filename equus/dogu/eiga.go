f08faf30ab3fed8f503ed276379caaed17d65bfd
AQ <aq@okaq.com>
2018-01-07 21:33:46 -0500

    List image file names from img/ directory

--- equus/dogu/eiga.go
+++ equus/dogu/eiga.go
@@ -8,7 +8,12 @@ package main
 import (
     "fmt"
     "net/http"
+    "os"
     "time"
+)
+
+var (
+    Images []string
 )
 
 const (
@@ -28,8 +33,25 @@ func ListHandler(w http.ResponseWriter, r *http.Request) {
     // list png image names in img dir
 }
 
+func Pop() {
+    // populate image file names
+    var err error
+    f0, err := os.Open(IMG)
+    if err != nil {
+        fmt.Println(err)
+    }
+    Images, err := f0.Readdirnames(0)
+    if err != nil {
+        fmt.Println(err)
+    }
+    for index, name := range Images {
+        fmt.Printf("File #%d:  %s\n", index, name)
+    }
+}
+
 func main() {
     fmt.Printf("okaq equus eiga start\n%s\n", time.Now().String())
+    Pop()
     http.HandleFunc("/", EigaHandler)
     http.HandleFunc("/list", ListHandler)
     http.ListenAndServe(":8080", nil)
